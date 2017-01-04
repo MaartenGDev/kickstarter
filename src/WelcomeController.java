55e732bd93cdf5928a4b111849afccc0e7e55ce2
MaartenGDev <maarten.grootoonk@gmail.com>
2017-01-04 17:55:06 +0100

    Added project page

--- src/WelcomeController.java
+++ src/WelcomeController.java
@@ -8,20 +8,31 @@ import javax.servlet.http.*;
 public class WelcomeController extends HttpServlet {
 
 
-    public void init(ServletConfig config) throws ServletException
-    {
+    public void init(ServletConfig config) throws ServletException {
         super.init(config);
     }
 
     public void doGet(HttpServletRequest request,
-            HttpServletResponse response)
-        throws ServletException, IOException
-    {
+                      HttpServletResponse response)
+            throws ServletException, IOException {
         response.setContentType("text/html");
 
         String message = "Hello Kickstarter";
 
         PrintWriter out = response.getWriter();
-        out.println("<h1>" + message + "</h1>");
+
+
+        out.println("<!doctype html>");
+        out.println("<html lang=\"en\">");
+        out.println("<head>");
+        out.println("<meta charset=\"UTF - 8\">");
+        out.println("<meta name=\"viewport\" content=\"width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0\">");
+        out.println("<meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">");
+        out.println("<title>Document</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello World</h1>");
+        out.println("</body>");
+        out.println("</html>");
     }
 }
